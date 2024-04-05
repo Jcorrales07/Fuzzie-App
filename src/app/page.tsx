ca516b73200d06e1e624947659677640af38caf9
Joe Corrales <corralesjoe07@gmail.com>
2024-04-04 22:11:43 -0600

    a ver

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,8 +33,8 @@ export default function Home() {
                                 <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
                                     Automate Your Work With Fuzzie
                                 </h1>
-                            </div>} children={''}>
-
+                            </div>}>
+                        <div></div>
                     </ContainerScroll>
                 </div>
             </section>
