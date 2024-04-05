9406028f039241fcb09b652a7da1fcbd723b4bb3
Joe Corrales <corralesjoe07@gmail.com>
2024-04-04 22:03:04 -0600

    Landing page terminada

---
+++ src/app/(main)/(pages)/layout.tsx
@@ -0,0 +1,15 @@
+// Esto es un diseño re usable para todas las carpetas (rutas) que estan dentro de (pages)
+
+import React from 'react'
+
+type Props = { children: React.ReactNode }
+
+const Layout = ({children}: Props) => {
+    return (
+        <div className={'border-l-[1px] border-t-[1px] pb-20 h-screen rounded-l-3xl border-muted-foreground/20 overflow-scroll'}>
+            {children}
+        </div>
+    )
+}
+
+export default Layout;
